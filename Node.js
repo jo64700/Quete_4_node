29af9d5646a9b23bb9ea86b2420a8433d2f3f8c8
jo64700 <jonathanbarone64@gmail.com>
2023-05-25 09:45:06 +0200

    second commit

---
+++ Node.js
@@ -0,0 +1,3 @@
+MESSAGE=`I am ${MY_NAME}, wilder in ${MY_CITY}, and I love ${MY_LANGUAGE}`;
+
+console.log(process.env.MESSAGE)
---
+++ server.js
@@ -0,0 +1,5 @@
+/*const port = process.env.SERVER_PORT;
+server.listen(port);
+*/
+
+console.log(process.env.TA_VARIABLE)
